e9cb108f025781aa640a5fa28f87faf61c787692
Ye Xianjin <advancedxy@gmail.com>
2015-01-07 22:46:12 +0800

    add solution to problem 21. It precomputes primes, all the divisors of number x which is under n, the sum of proper divisors. When querying, it iterate through all the number under query number to find out weather it's an amicable number. I don't think this piece of code as time efficient and the hackerrank site evalutes their tests in about 2~4s. The worst case is about 3.88s, which is pretty bad. Should consider another solution.

---
+++ project-euler/problem-21/AmicableNumbers.scala
@@ -0,0 +1,47 @@
+object Solution {
+
+  def sievePrimeGenerator(n: Int): (List[Int], Array[Boolean]) = {
+    val nums = Array.fill(n + 1)(true)
+    val primes = for (i <- (2 to n).toList if nums(i)) yield {
+      var j = 2
+      while (i * j <= n) {
+        nums(i * j) = false
+        j += 1
+      }
+      i
+    }
+    (primes, nums)
+  }
+
+  def divisorGenerator(n: Int): Array[Vector[Int]] = {
+    val result = Array.fill(n + 1)(Vector.empty[Int])
+    val (primes, primeTable) = sievePrimeGenerator(n)
+    for (i <- 1 to n) {
+      if (primeTable(i)) result(i) = Vector(1, i)
+      else {
+        val p = primes.dropWhile(i % _ != 0).head
+        result(i) = (result(i / p) ++ result(i / p).map(p * _)).distinct
+      }
+    }
+    result
+  }
+
+  def main(args: Array[String]) {
+    val upperBound = 2e5.toInt
+    val divisorMap = divisorGenerator(upperBound)
+    val sumOfProperDivisorMap = divisorMap.zipWithIndex.map { case (x, y) =>
+      x.sum - y
+    }
+
+    val t = readLine.toInt
+    for (i <- 1 to t) {
+      val n = readLine.toInt
+      val pairs = for {
+        j <- 2 until n
+        if sumOfProperDivisorMap(j) != j && sumOfProperDivisorMap(j) < upperBound &&
+          sumOfProperDivisorMap(sumOfProperDivisorMap(j)) == j
+      } yield j
+      println(pairs.sum)
+    }
+  }
+}
