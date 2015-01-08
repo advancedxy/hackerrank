495456c00166dbec1644c903a4f0873c9e4df0c7
Ye Xianjin <advancedxy@gmail.com>
2015-01-08 09:11:36 +0800

    using prime factor map to represent the number. Using formula to calculate sum of proper divisors. Worst case is 1.23s.

--- project-euler/problem-21/AmicableNumbers.scala
+++ project-euler/problem-21/AmicableNumbers.scala
@@ -26,15 +26,34 @@ object Solution {
     result
   }
 
-  def main(args: Array[String]) {
-    val upperBound = 2e5.toInt
-    val divisorMap = divisorGenerator(upperBound)
-    val sumOfProperDivisorMap = divisorMap.zipWithIndex.map { case (x, y) =>
-      x.sum - y
+  def primeFactorMap(n: Int): Array[Map[Int, Int]] = {
+    val result = Array.fill(n + 1)(Map.empty[Int, Int])
+    val (primes, primeTable) = sievePrimeGenerator(n)
+    for (i <- 2 to n) {
+      if (primeTable(i)) result(i) =  Map(i -> 1)
+      else {
+        val p = primes.dropWhile(i % _ != 0).head
+        val pExp = result(i / p).getOrElse(p, 0)
+        result(i) = result(i / p).updated(p, pExp + 1)
+      }
     }
+    result
+  }
 
+  def main(args: Array[String]) {
+    val upperBound = 1e5.toInt
+    //val divisorMap = divisorGenerator(upperBound)
+    //val sumOfProperDivisorMap = divisorMap.zipWithIndex.map { case (x, y) =>
+      //x.sum - y
+    //}
+
+    val primeFactorMapArray  = primeFactorMap(upperBound)
+    val sumOfProperDivisorMap = primeFactorMapArray.zipWithIndex map {
+      case (x, y) =>
+        (x.foldLeft(1.0) { (product, pf) => product * (math.pow(pf._1, pf._2 + 1) - 1) / (pf._1 - 1) }).toInt - y
+    }
     val amicableNumbers = for {
-      j <- 2 until upperBound / 2
+      j <- 2 until upperBound
       if sumOfProperDivisorMap(j) != j && sumOfProperDivisorMap(j) < upperBound &&
         sumOfProperDivisorMap(sumOfProperDivisorMap(j)) == j
     } yield j
