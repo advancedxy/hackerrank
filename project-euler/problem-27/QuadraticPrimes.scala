1a68430871666d7b7fc5417a8c00143b7c552da5
Ye Xianjin <advancedxy@gmail.com>
2015-01-12 21:55:51 +0800

    add solution to problem 27. There is some smell code. Review the code later.

--- project-euler/problem-27/QuadraticPrimes.scala
+++ project-euler/problem-27/QuadraticPrimes.scala
@@ -1,10 +1,10 @@
 object Solution {
   val upperBound = 2000
-  val indexArray = Array.fill(upperBound + 1)(0)
+  val abArray = Array.fill(upperBound + 1)((0, 0, 0))
 
-  def sievePrimeGenerator(n: Int): Array[Int] = {
+  def sievePrimeGenerator(n: Int): Array[Boolean] = {
     val nums = Array.fill(n + 1)(true)
-    val primes = for (i <- (2 to n).toIterator if nums(i)) yield {
+    val primes = for (i <- (2 to n) if nums(i)) yield {
       var j = 2
       while (i * j <= n) {
         nums(i * j) = false
@@ -12,21 +12,38 @@ object Solution {
       }
       i
     }
-    primes.toArray
+    nums
   }
 
   def caculteIndexes {
     val primes = sievePrimeGenerator(upperBound * 80 * 2 + upperBound)
     def consecutivePrimesCount(a: Int, b: Int): Int = {
-      Stream.from(0).map(x => x * x + a * x + b).
-        takeWhile(x => x >= 2 && primes(x)).count
+      Stream.from(0).map(x => x * x + a * x + b)
+        .takeWhile(x => x >= 2 && primes(x)).length
     }
-    var max = 0
+    var max,a,b = 0
     for (index <- 0 to upperBound) {
-      for (i <- 0 until index)
+      for (i <- -index to index) {
+        val c1 = consecutivePrimesCount(index, i)
+        if (c1 > max) {
+          max = c1
+          a = index
+          b = i
+        }
+        val c2 = consecutivePrimesCount(i, index)
+        if (c2 > max) {
+          max = c2
+          a = i
+          b = index
+        }
+      }
+      abArray(index) = (a, b, max)
     }
   }
   def main(args: Array[String]) {
-
+    caculteIndexes
+    val n = readLine.toInt
+    val (a, b, _) = abArray(n)
+    println(a + " " + b)
   }
 }
