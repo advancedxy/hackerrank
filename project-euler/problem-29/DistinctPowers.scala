078716cc1211dea102a26b5f2cb0156cc0a317b1
Ye Xianjin <advancedxy@gmail.com>
2015-01-18 18:05:40 +0800

    Add a new method to calculate duplicate number. This version is both time and space more efficient than the previous version because I limit the set size to b (or N). The previous version's set size will be O(blogb). And due to test cases in hackerrank, this version's worst case is 1.35s vs 3.69s(previous version). Next thing needs to be done is the intLog function. It should be more efficient.

--- project-euler/problem-29/DistinctPowers.scala
+++ project-euler/problem-29/DistinctPowers.scala
@@ -1,20 +1,23 @@
 object Solution {
 
+  def calculateDuplicates(b: Int, n: Int): Array[Int] = {
+    val duplicates = for (i <- 2 to n) yield {
+      var set = Set.empty[Int]
+      for (j <- 1 until i) {
+        set ++= (2 to j * b / i).filter(x => (i * x) % j == 0)
+      }
+      set.size
+    }
+    (duplicates.foldLeft(List(0)) { case (x, y) =>
+      y + x.head :: x
+    }).reverse.drop(1).toArray
+  }
+
   def distinctPowers(a: Int, b: Int): Long = {
     var total = 1l * (a - 1) * (b - 1)
     val sqrta = math.sqrt(a).toInt
     val log2a = (math.log(a) / math.log(2)).toInt
-    val baseSet = (2 to b).toSet
-    var set = baseSet
-    val duplicates = for (i <- 2 to log2a) yield {
-      val newSet = baseSet.map(_ * i)
-      val previousCount = set.size
-      set = set ++ newSet
-      previousCount + newSet.size - set.size
-    }
-    val duplicatesSum = (duplicates.foldLeft(List(0)) { case (x, y) =>
-      y + x.head :: x
-    }).reverse.drop(1).toVector
+    val duplicatesSum = calculateDuplicates(b, log2a)
     val visted = scala.collection.mutable.Set.empty[Int]
     for (i <- 2 to sqrta if !visted(i)) {
       val logia = intLog(a, i)
