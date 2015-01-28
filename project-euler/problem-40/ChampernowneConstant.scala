ae32842fe20ecf046e5cb59311a2e04e19abae97
Ye Xianjin <advancedxy@gmail.com>
2015-01-28 10:38:42 +0800

    Add solution to problem 40. It's quite stupid to find WA only because I used toInt to round math.pow(10, n - 1). There may 2 optimizations for this code. 1. indexWhere will be linear even there are only 19 elements to search. 2. math.pow may be slow. Should use a table to cache result.

--- project-euler/problem-40/ChampernowneConstant.scala
+++ project-euler/problem-40/ChampernowneConstant.scala
@@ -4,16 +4,29 @@ object Solution {
     val start = x.head._2 + 1
     val numbers = math.pow(10, y).toLong - math.pow(10, y - 1).toLong
     val end = start + y * numbers - 1
-    (start, end) :: x
+    (start, if (end > 0) end else Long.MaxValue) :: x
   }).reverse.toVector
 
   def digitOfIndex(idx: Long): Int = {
-    val n = idx.toString.length
-    val (start, _) = headTailTable(n)
-    val (idx)
+    val n = headTailTable.indexWhere(x => idx >= x._1 && idx <= x._2)
+    val (start, end)= headTailTable(n)
+    val numberStart = math.pow(10, n - 1).toLong - 1
+    val number = idx - start + 1
+    val (d, p) = (number / n + numberStart, number % n)
+    if (p == 0) (d % 10).toInt
+    else {
+      var c = 0l
+      var digits = d + 1
+      for (i <- p to n) {c = digits % 10; digits /= 10}
+      c.toInt
+    }
   }
+
   def main(args: Array[String]) {
-
-
+    val t = readLine.toInt
+    for (i <- 1 to t) {
+      val digits = readLine.split(" ").map(x => digitOfIndex(x.toLong))
+      println(digits.product)
+    }
   }
 }
