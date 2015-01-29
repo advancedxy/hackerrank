ec5abf8cbf866e8257d5d79ee82d2dc696d469b6
Ye Xianjin <advancedxy@gmail.com>
2015-01-29 12:31:45 +0800

    Do not generate all the pentagon numbers. And the worst case time drop from 3.66 to 0.47 .

--- project-euler/problem-44/PentagonNumbers.scala
+++ project-euler/problem-44/PentagonNumbers.scala
@@ -4,14 +4,21 @@ object Solution {
     val result = (1l to n).map(x => x * (3 * x - 1) / 2).toArray
     (result, result.toSet)
   }
+
+  def isPentagonNumber(x: Long): Boolean = {
+    val n = (math.sqrt(24.0 * x + 1) + 1) / 6
+    n == n.toLong
+  }
   
   def main(args: Array[String]) {
     val Array(n, k) = readLine.split(" ").map(_.toInt)
-    val (penNum, penNumSet) = genPentagonNumbers(n)
-    val penNums = (k + 1 until n).filter({ x =>
-      val (pn, pk) = (penNum(x), penNum(x - k))
-      penNumSet(pn + pk) || penNumSet(pn - pk)
-    }).map(penNum(_))
-    println(penNums.mkString("\n"))
+    var i = (k + 1).toLong
+    var pk,pn = 0L
+    while (i < n) {
+      pn = i * (3 * i - 1) / 2
+      pk = (i - k) * (3 * (i - k) - 1) / 2
+      if (isPentagonNumber(pn + pk) || isPentagonNumber(pn - pk)) println(pn)
+      i += 1
+    }
   }
 }
