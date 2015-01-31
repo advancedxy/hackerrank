2316f66d8bbb181a2501e17d8f1ebfc35770cdc9
Ye Xianjin <advancedxy@gmail.com>
2015-01-31 23:49:02 +0800

    Update solution to problem 47. There is no need to store the whole prime factor map. we can just update the number of prime factors. And change the way to print nums, it's much less efficient than where. Should think another way. However, this passes the hackerrank test.

--- project-euler/problem-47/DistinctPrimesFactors.scala
+++ project-euler/problem-47/DistinctPrimesFactors.scala
@@ -1,10 +1,10 @@
 object Solution {
 
-  def sievePrimeGenerator(n: Int): (List[Int], Array[Boolean]) = {
+  def sievePrimeGenerator(n: Int): (Array[Int], Array[Boolean]) = {
     val nums = Array.fill(n + 1)(true)
     nums(0) = false
     nums(1) = false
-    val primes = for (i <- (2 to n).toList if nums(i)) yield {
+    val primes = for (i <- (2 to n) if nums(i)) yield {
       var j = 2
       while (i * j <= n) {
         nums(i * j) = false
@@ -12,44 +12,29 @@ object Solution {
       }
       i
     }
-    (primes, nums)
+    (primes.toArray, nums)
   }
 
-  def primeFactorMap(n: Int): Array[Map[Int, Int]] = {
-    val result = Array.fill(n + 1)(Map.empty[Int, Int])
+  def genPrimeFactorNum(n: Int): Array[Int] = {
+    val result = Array.fill(n + 1)(1)
     val (primes, primeTable) = sievePrimeGenerator(n)
-    for (i <- 2 to n) {
-      if (primeTable(i)) result(i) =  Map(i -> 1)
-      else {
-        val p = primes.dropWhile(i % _ != 0).head
-        val pExp = result(i / p).getOrElse(p, 0)
-        result(i) = result(i / p).updated(p, pExp + 1)
-      }
+    for (i <- 2 to n if !primeTable(i)) {
+      val index = primes.indexWhere(i % _ == 0)
+      val p = if (index < 0) 1 else primes(index) // p will never be 1
+      val pp = i / p
+      result(i) = if (pp % p == 0) result(i / p) else result(i / p) + 1
     }
-    result(1) = Map(1 -> 1)
     result
   }
   
   def main(args: Array[String]) {
     val Array(n, k) = readLine.split(" ").map(_.toInt)
-    val primeFactors = primeFactorMap(n + k)
-    val nums = (2 until n + k).filter(primeFactors(_).size == k)
-    var i = 1
-    var cnt = 1
-    while(i < nums.size) {
-      if (nums(i) - nums(i - 1) == 1) {
-        cnt += 1
-      } else {
-        for (j <- 0 to cnt - k) {
-          println(nums(j - cnt + i))
-        }
-        cnt = 1
-      }
-      i += 1
+    val numOfPrimeFactors = genPrimeFactorNum(n + k)
+    val nums = (6 until n + k).filter { x =>
+      numOfPrimeFactors(x) == k
     }
-    for (j <- 0 to cnt -k) {
-      println(nums(i - cnt + j))
+    for (ns <- nums.sliding(k)) {
+      if (ns.zip(ns.slice(1, ns.size)).forall(x => x._2 - x._1 == 1)) println(ns(0))
     }
   }
-
 }
