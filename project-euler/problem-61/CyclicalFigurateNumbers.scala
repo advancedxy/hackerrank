737792263cb5b8b3f0afbf3a20ec5bc19281ff2f
Ye Xianjin <advancedxy@gmail.com>
2015-02-06 00:30:13 +0800

    Add solution to problem 61.

--- project-euler/problem-61/CyclicalFigurateNumbers.scala
+++ project-euler/problem-61/CyclicalFigurateNumbers.scala
@@ -18,35 +18,42 @@ object Solution {
   }
 
   def genCyclics(comb: Array[Int]) = {
-    
+    import scala.collection.mutable.LinkedHashSet
+
     def isPoly(n: Int) = comb.exists(s => isNgonal(s, n))
     def slides(n: Int) = comb collect { case s if isNgonal(s, n) => s }
 
     val nums = comb.toList.map({ n =>
       (1000 to 9999).filter(isNgonal(n, _)).toList
     })
-
-    def genCombines(ls: List[List[Int]]): List[List[Int]] = ls match {
-      case Nil => Nil
-      case head :: Nil => head.map(List(_))
-      case head :: tail =>
-        for {
-          h <- head
-          ls <- genCombines(tail) if h % 100 == ls.head / 100
-        } yield h :: ls
-    }
-
-    def isCyclics(x: Array[Int]) = {
-      val last = x(x.size - 1)
-      (x(0) / 100 == last % 100) && x.zip(x.slice(1, x.size)).forall { y =>
-        y._1 % 100 == y._2 / 100
+    
+    val resultSet = LinkedHashSet.empty[List[Int]]
+    
+    def searchNums(result: LinkedHashSet[Int], vistedCategory: Set[Int]) {
+      for {
+        j <- 0 until comb.size if !vistedCategory(j)
+        ele <- nums(j) if ele / 100 == result.last % 100 && !result(ele)
+      } {
+        val newResult = result + ele
+        if (newResult.size == comb.size) {
+          if (newResult.head / 100 == newResult.last % 100)
+            resultSet.add(newResult.toList)
+        }
+        else {
+          searchNums(newResult, vistedCategory + j)
+        }
       }
     }
 
+    for (ele <- nums(0)) searchNums(LinkedHashSet(ele), Set(0))
+
+    resultSet
   }
 
   def main(args: Array[String]) {
     val _ = readLine
     val comb = readLine.split(" ").map(_.toInt)
+    val result = genCyclics(comb).map(_.sum).toList.sorted
+    result foreach println
   }
 }
