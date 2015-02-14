8241a588c38e8171aac9e6461d8aca75bbd05e77
Ye Xianjin <advancedxy@gmail.com>
2015-02-14 09:46:10 +0800

    Change combinations to for comprehension. It runs 0.8xS for worst cases. It may run faster if I don't use BigInt and handcraft a bitCount method. But I don't think it's worthy.

--- algorithms/warmup/ACMICPCTeam.scala
+++ algorithms/warmup/ACMICPCTeam.scala
@@ -3,9 +3,11 @@ object Solution {
   def main(args: Array[String]) {
     val Array(n, m) = readLine.split(" ").map(_.toInt)
     val nums = (1 to n).map(_ => BigInt(readLine, 2))
-    val counts = nums.combinations(2).map( { case Vector(x, y) =>
-      (x | y).bitCount
-    } ).toList
+    val counts = for {
+      i <- 0 until nums.size
+      j <- i + 1 until nums.size
+    } yield (nums(i) | nums(j)).bitCount
+
     val maxCount = counts.max
     val maxCountC = counts.filter(_ == maxCount).size
     println(maxCount)
