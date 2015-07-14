8643c52f388a65ff159417bc923fadc02fabefaa
Xianjin Ye <advancedxy@gmail.com>
2015-07-14 23:59:01 +0800

    Add a cache strategy to speed up the newton method's convergence. I tested the program on my local machine, it took about 11 iterations to get 10000 digits precision. After this cache speedup, the program still need 11 iterations to compute prime's BigDecimal sqrt. Other numbers normally take 1-2 iterations, which is a quite speedup. In my opinion, It will speed more for larger input n. However, I don't think we should do this type of optimization, the time on hackerrank should be longger. The newton method is quite good already. Another thing to note: python's decimal is really faster than java's. I should look at the python's decimal source code for some insight.

--- project-euler/problem-80/SquareRootDigitalExpansion.scala
+++ project-euler/problem-80/SquareRootDigitalExpansion.scala
@@ -1,34 +1,49 @@
-object Solution {
+bject Solution {
   import java.math.BigDecimal.ROUND_HALF_UP
   import java.math.BigDecimal
 
-  def squareRoot(i: Int, nDigits: Int): BigInt = {
-    val limit = BigInt(10).pow(nDigits + 1)
-    var a = 5 * BigInt(i)
-    var b = BigInt(5)
-    while (b < limit) {
-      if (a >= b) {
-        a -= b
-        b += 10
+  def sievePrimeGenerator(n: Int): (Array[Int], Array[Boolean]) = {
+    val primeTable = Array.fill(n + 1)(true)
+    primeTable(0) = false
+    primeTable(1) = false
+    val primes = for (i <- 2 to n if primeTable(i)) yield {
+      var j = 2
+      while (i * j <= n) {
+        primeTable(i * j) = false
+        j += 1
       }
-      else {
-        a *= 100
-        b = 10 * b - 45
-      }
+      i
     }
-    b / 100
+    (primes.toArray, primeTable)
   }
+
+  // the maximum n can only be 1000
+  val nLimit = 1000
+  val (primes, primeTable) = sievePrimeGenerator(nLimit)
+  val bigDecimalCache = (0 to nLimit).map(x => BigDecimal.valueOf(x)).toArray
 
   val ZERO = new BigDecimal("0")
   val TWO = new BigDecimal("2")
   def sqrtBigDecimal(A: BigDecimal, SCALE: Int) = {
+    val aInt = A.intValue
     var x0 = ZERO
     var x1 = new BigDecimal(math.sqrt(A.intValue));
+    if (! primeTable(aInt)) {
+      val base = primes.find(aInt % _ == 0).get
+      val rest = aInt / base
+      if (isPerfectRoot(rest)){
+        val sqrt = math.sqrt(rest).toInt
+        x1 = bigDecimalCache(base).multiply(BigDecimal.valueOf(sqrt)).setScale(SCALE, ROUND_HALF_UP)
+      } else{
+        x1 = bigDecimalCache(base).multiply(bigDecimalCache(rest)).setScale(SCALE, ROUND_HALF_UP)
+      }
+    }
     while (!x0.equals(x1)) {
         x0 = x1
         x1 = A.divide(x0, SCALE, ROUND_HALF_UP).add(x0)
         x1 = x1.divide(TWO, SCALE, ROUND_HALF_UP)
     }
+    bigDecimalCache(aInt) = x1
     x1
   }
 
