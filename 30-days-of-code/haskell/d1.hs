497778241ff1cf4a0ff183a63ad787dc5c2b000a
Xianjin YE <advancedxy@gmail.com>
2016-01-05 21:34:22 +0800

    Add solution to d1.

---
+++ 30-days-of-code/haskell/d1.hs
@@ -0,0 +1,12 @@
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+main :: IO()
+main = do
+  putStrLn "Primitive : double"
+  putStrLn "Primitive : char"
+  putStrLn "Primitive : boolean"
+  putStrLn "Primitive : int"
+  putStrLn "Referenced : String"
+  putStrLn "Primitive : boolean"
+  putStrLn "Primitive : double"
+  putStrLn "Primitive : char"
+  putStrLn "Referenced : String"
